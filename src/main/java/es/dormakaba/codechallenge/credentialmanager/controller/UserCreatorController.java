04dda616fed28c05d8adb02e1b48e28648616625
Sergio Rodrigalvarez <sergio.rodrigalva@gmail.com>
2024-10-04 00:37:14 +0200

    refactor: return 201 on User creation

--- src/main/java/es/dormakaba/codechallenge/credentialmanager/controller/UserCreatorController.java
+++ src/main/java/es/dormakaba/codechallenge/credentialmanager/controller/UserCreatorController.java
@@ -1,9 +1,11 @@
 package es.dormakaba.codechallenge.credentialmanager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.dormakaba.codechallenge.credentialmanager.application.UserCreator;
@@ -20,6 +22,7 @@ public class UserCreatorController {
     private UserCreator userCreator;
     
     @PostMapping("/user")
+    @ResponseStatus(HttpStatus.CREATED)
     public @ResponseBody UserCreatorResponse create(@RequestBody UserCreatorRequest request) throws ValidationException, UserAlreadyExistsException {
         User user = userCreator.create(request.name);
         return new UserCreatorResponse(user.getId(), user.getName());
