6fcb7de7fecf2ecefde15d6f9be39ed4269b9a00
Sergio Rodrigalvarez <sergio.rodrigalva@gmail.com>
2024-10-05 01:47:32 +0200

    feat: capture HttpMessageNotReadableException and format informative message

--- src/main/java/es/dormakaba/codechallenge/shared/controller/DefaultExceptionAdvice.java
+++ src/main/java/es/dormakaba/codechallenge/shared/controller/DefaultExceptionAdvice.java
@@ -3,6 +3,7 @@ package es.dormakaba.codechallenge.shared.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,5 +18,12 @@ public class DefaultExceptionAdvice {
     public String defaultExceptionHandler(Exception e) {
         logger.info(e.toString());
         return "An unexpected error occurred on the server. Please try again later. If the problem persists, contact support.";
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String httpMessageNotReadableException(HttpMessageNotReadableException e) {
+        String errorMessage = e.getMessage();
+        return errorMessage.substring(0, errorMessage.indexOf(':'));
     }
 }
